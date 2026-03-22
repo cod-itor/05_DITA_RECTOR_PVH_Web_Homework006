458e89484d978b13b1cb223d45a46285e076055c
Dita rector <rectordita@gmail.com>
2026-03-22 15:05:26 +0700

    NavBar layout Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,33 @@
 "use client";
 
 import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
-import { Menu, X } from "lucide-react";
+import { Menu, BellDot,ShoppingCart } from "lucide-react";
+import { UserProfile } from "./UserProfile";
 
 export function Navbar() {
   const { state } = useSidebar();
 
   return (
     <header className="sticky top-0 z-40 border-b border-slate-200 bg-white shadow-sm">
-      <div className="flex h-16 items-center justify-between px-6 py-4">
-        <div className="flex items-center gap-4">
+      <div className="flex h-16 items-center flex-row-reverse  px-6 py-4">
+        {/* <div className="flex items-center gap-4">
+          this sidebar trigger can be removed 
           <SidebarTrigger className="group rounded-lg border border-slate-200 px-3 py-2 transition-all hover:bg-slate-100 hover:border-slate-300">
             <Menu className="h-5 w-5 text-black hover:text-slate-900" />
           </SidebarTrigger>
-
-          <div className="hidden sm:block">
-            <h1 className="text-lg font-bold text-slate-900">
-              HRD Admin Dashboard
-            </h1>
-            <p className="text-xs text-slate-500">
-              {state === "expanded" ? "Sidebar Expanded" : "Sidebar Collapsed"}
-            </p>
-          </div>
-        </div>
+        </div> */}
 
         <div className="flex items-center gap-4">
-          <button className="rounded-lg bg-cyan-500 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-cyan-600 hover:shadow-md active:scale-95">
-            Help
+        
+          <button className="relative rounded-lg p-2 hover:bg-slate-100 transition-colors">
+            <BellDot className="h-5 w-5 text-slate-600 hover:text-slate-900" />
           </button>
+          <button className="relative rounded-lg p-2 hover:bg-slate-100 transition-colors">
+            <ShoppingCart className="h-5 w-5 text-slate-600 hover:text-slate-900" />
+          </button>
+          <UserProfile/>
+          
+        
         </div>
       </div>
     </header>
