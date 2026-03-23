4a45002cff9e1dcf52c037691d4f88e951efcc5a
Dita rector <rectordita@gmail.com>
2026-03-24 02:51:45 +0700

    Fetched Product

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
-export default function page() {
+export default async function page() {
+
+const response = await fetch('https://homework-api.noevchanmakara.site/api/v1/products');
+const product = await response.json();
+const products  = await product.payload;
+
+
+
+
   return (
     <div className='flex'>
       <h1>List of All Product</h1>
       <form action="search">
         <input type="text" placeholder=''/>
       </form>
+
+
+
       
     </div>
   )
