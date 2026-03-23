2c15047fe63b99d0cd366ffab10560d09fe98d12
Dita rector <rectordita@gmail.com>
2026-03-24 02:41:36 +0700

    Fetched successfully

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      products Page 
+    <div className='flex'>
+      <h1>List of All Product</h1>
+      <form action="search">
+        <input type="text" placeholder=''/>
+      </form>
+      
     </div>
   )
 }
--- src/components/CustomerHomepageCardComponent.jsx
+++ src/components/CustomerHomepageCardComponent.jsx
@@ -1,8 +1,17 @@
 "use client";
 
-export default function CustomerHomepageCardComponent() {
+import Image from "next/image";
+import Link from "next/link";
+import { Users, ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+const image = "/img/ladyHoldingLaptop.png"; // Update this path to match your actual image
+
+export default function CustomerHomepageCardComponent({customer}) {
+  console.log(customer.payload.length)
+  
   return (
-    <div className="group relative w-full max-w-md h-[700px] bg-gradient-to-br from-amber-50 to-white rounded-[2.5rem] p-8 flex flex-col justify-between overflow-hidden border border-amber-100 shadow-[0_20px_50px_rgba(245,158,11,0.05)] transition-all duration-500 hover:shadow-[0_40px_80px_rgba(245,158,11,0.1)] hover:-translate-y-2">
+      <div className="group relative w-full max-w-md h-[700px] bg-gradient-to-br from-amber-50 to-white rounded-[2.5rem] p-8 flex flex-col justify-between overflow-hidden border border-amber-100 shadow-[0_20px_50px_rgba(245,158,11,0.05)] transition-all duration-500 hover:shadow-[0_40px_80px_rgba(245,158,11,0.1)] hover:-translate-y-2">
       <div className="absolute -top-20 -right-20 w-64 h-64 bg-amber-200/30 rounded-full blur-3xl group-hover:bg-amber-300/40 transition-colors duration-700" />
       <div className="relative z-10 flex flex-col gap-1">
         <div className="flex items-center gap-2">
@@ -22,6 +31,8 @@ export default function CustomerHomepageCardComponent() {
         <Image
           src={image}
           alt="customer illustration"
+          width={200}
+          height={200}
           className="w-full h-auto drop-shadow-[0_35px_35px_rgba(245,158,11,0.3)] object-contain"
           priority
         />
@@ -31,9 +42,9 @@ export default function CustomerHomepageCardComponent() {
           <span className="text-[10px] uppercase font-bold text-slate-400 tracking-widest">
             Total Users
           </span>
-          <span className="text-2xl font-black text-slate-800">{""}</span>
+          <span className="text-2xl font-black text-slate-800">{customer.payload.length}</span>
         </div>
-        <Link>
+        <Link href="/customer">
           <Button className="group/btn bg-amber-300 hover:bg-amber-500 text-white h-14 px-8 rounded-2xl font-bold shadow-lg shadow-amber-200 transition-all active:scale-95 flex gap-2">
             View All
             <ArrowRight className="size-4 transition-transform group-hover/btn:translate-x-1" />
@@ -41,5 +52,7 @@ export default function CustomerHomepageCardComponent() {
         </Link>
       </div>
     </div>
+
+    
   );
 }
