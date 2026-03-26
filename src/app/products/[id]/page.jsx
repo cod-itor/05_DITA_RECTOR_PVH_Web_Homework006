7f5284b5e2cac9388073cbf265362d68f1e7ee40
Dita rector <rectordita@gmail.com>
2026-03-26 07:26:31 +0700

    Fetching have problem

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,20 +1,29 @@
 import ProductDetails from "@/components/ProductDetails";
 import { notFound } from "next/navigation";
 
-
 export default async function ProductPage({ params }) {
-  const {productId} = await params;
+  const { productId } = params;
 
-   const res = await fetch(`https://homework-api.noevchanmakara.site/api/v1/products/${productId}`);
-  const products = await res.json();
+  const res = await fetch(
+    `https://homework-api.noevchanmakara.site/api/v1/products/${productId}`,
+  );
 
+  if (!res.ok) {
+    
+    return notFound();
+  }
+
+  const data = await res.json();
+
+  const product = Array.isArray(data.payload)
+    ? data.payload[0]
+    : data.payload || null;
+
+  if (!product) return notFound();
 
   return (
-    <>
     <div className="min-h-screen bg-slate-50 py-12">
-      <ProductDetails product={products.payload} />
+      <ProductDetails product={product} />
     </div>
-    
-    </>
   );
 }
